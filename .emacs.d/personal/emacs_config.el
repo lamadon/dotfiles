0519843035ffa60d54931bb4e177e03d87f7e6a1
Jean-Luc Lamadon <jean-luc.lamadon@enevo.com>
2017-08-16 00:18:23 +0300

    .emacs config

---
+++ .emacs.d/personal/emacs_config.el
@@ -0,0 +1,26 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; basic emacs configuration to be used in conjunction with prelude       ;;
+;; pragmaticemacs.com/installing-and-setting-up-emacs/                    ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;Add MELPA repository for packages
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; prelude options                                                        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; install additional packages - add anyto this list that you want to
+;; be installed automatically
+(prelude-require-packages '(multiple-cursors ess))
+
+;;enable arrow keys
+(setq prelude-guru nil)
+
+;;smooth scrolling
+(setq prelude-use-smooth-scrolling t)
+
+;;uncomment this to use default theme
+;;(disable-theme 'zenburn)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
